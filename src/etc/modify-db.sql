8bcc09ea38537f54316ae8c03ae061f59c1a763f
Stefan Berggren <nsg@nsg.cc>
2023-06-11 18:18:16 +0200

    WIP
    This more or less works but the need of this hack shows that I should
    probably have chosen another path.

--- src/etc/modify-db.sql
+++ src/etc/modify-db.sql
@@ -1,3 +1,4 @@
+-- A audit lof of deleted assets
 CREATE TABLE IF NOT EXISTS assets_delete_audits (
     id INT GENERATED ALWAYS AS IDENTITY,
     asset_id UUID NOT NULL,
@@ -7,6 +8,7 @@ CREATE TABLE IF NOT EXISTS assets_delete_audits (
     changed_on TIMESTAMP(6) NOT NULL
 );
 
+-- A lookup table for assets that are imported from the file system via sync feature
 CREATE TABLE IF NOT EXISTS assets_filesync_lookup (
     id INT GENERATED ALWAYS AS IDENTITY,
     user_id VARCHAR(256) NULL,
@@ -15,6 +17,17 @@ CREATE TABLE IF NOT EXISTS assets_filesync_lookup (
     UNIQUE(user_id, asset_path)
 );
 
+-- A queue used by the sync feature to import assets from the file system
+CREATE TABLE IF NOT EXISTS sync_work_queue (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    user_id VARCHAR(256) NULL,
+    identifier VARCHAR(256) NULL,
+    payload TEXT NOT NULL,
+    completed BOOLEAN DEFAULT FALSE,
+    changed_on TIMESTAMP(6) NOT NULL
+);
+
+-- Function that logs the deletion of assets
 CREATE OR REPLACE FUNCTION log_assets_delete_audits()
     RETURNS TRIGGER
     LANGUAGE PLPGSQL
@@ -27,6 +40,7 @@ BEGIN
 END;
 $$;
 
+-- Trigger that calls the function above on deletetion of assets
 CREATE OR REPLACE TRIGGER trigger_assets_delete_audits
 BEFORE DELETE ON assets
 FOR EACH ROW
