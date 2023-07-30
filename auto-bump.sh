f5a28fa3e86f9a78f998b004c575d782c83835c6
Stefan Berggren <nsg@nsg.cc>
2023-07-30 21:47:29 +0200

    Formatting tweaks

--- auto-bump.sh
+++ auto-bump.sh
@@ -67,11 +67,12 @@ fi
 echo "
 A new version of Immich has been released. :tada:
 
-This issue has been created to track the release of Immich **$NEW_VERSION**.
+This issue has been created to track the release of Immich **$NEW_VERSION_MAJOR_MINOR**.
 Feel free to add any comments or questions related to the release here.
 
 ## Upstream release notes
-https://github.com/immich-app/immich/releases/tag/$NEW_VERSION
+* https://github.com/immich-app/immich/releases/tag/$NEW_VERSION
+* https://github.com/immich-app/immich/releases
 
 ## When will this be released?
 Expect it to be released in the next few days to the beta channel, and to stable a few days later.
