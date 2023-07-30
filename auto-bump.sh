6ab0cbae2f77c86065344a5e11127d138b302d77
Stefan Berggren <nsg@nsg.cc>
2023-07-30 22:50:24 +0200

    Only add release link if it's missing

--- auto-bump.sh
+++ auto-bump.sh
@@ -83,8 +83,12 @@ ISSUE_NUMBER="$(get_issue_number "${NEW_VERSION_MAJOR_MINOR}")"
 if [ ! -z $ISSUE_NUMBER ]; then
     echo "Issue for $NEW_VERSION_MAJOR_MINOR already exists, issue $ISSUE_NUMBER."
     gh issue view $ISSUE_NUMBER --json body --jq ".body" > /tmp/ISSUE-BODY.md
-    sed -i "/https.*releases\/tag\/v.*/a https://github.com/immich-app/immich/releases/tag/$NEW_VERSION" /tmp/ISSUE-BODY.md
-    gh issue edit $ISSUE_NUMBER --body-file /tmp/ISSUE-BODY.md
+    if grep -qE "https.*releases\/tag\/$NEW_VERSION" /tmp/ISSUE-BODY.md; then
+        echo "Release link for $NEW_VERSION already exists."
+    else
+        sed -i "/https.*releases\/tag\/v.*/a https://github.com/immich-app/immich/releases/tag/$NEW_VERSION" /tmp/ISSUE-BODY.md
+        gh issue edit $ISSUE_NUMBER --body-file /tmp/ISSUE-BODY.md
+    fi
 else
     gh issue create \
         --title "Immich $NEW_VERSION_MAJOR_MINOR released" \
