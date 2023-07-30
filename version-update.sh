c3e8fd82879ebe780912ae7bfcfb2c22db7d62e9
Stefan Berggren <nsg@nsg.cc>
2023-07-30 11:54:08 +0200

    Add a note if there is new versions of the cli

--- version-update.sh
+++ version-update.sh
@@ -10,3 +10,8 @@ sed -i "s/$current_version/$latest_version/g" snap/snapcraft.yaml
 sed -i "s/$current_version/$latest_version/g" parts/machine-learning/Makefile
 sed -i "s/$current_version/$latest_version/g" patches/Makefile
 sed -i "s/$current_version/$latest_version/g" VERSION
+
+latest_immich_cli_version=$(curl -s https://api.github.com/repos/immich-app/CLI/releases/latest | jq -r '.tag_name')
+if ! grep -q $latest_immich_cli_version snap/snapcraft.yaml; then
+    echo "Note, new immich-cli version available: $latest_immich_cli_version"
+fi
