5350db53da6ae9b72c95c0ab8b33cae8031978c7
Stefan Berggren <nsg@nsg.cc>
2023-09-23 17:01:42 +0200

    New metadata extraction do not pad fractions

--- tests/tests_selenium.py
+++ tests/tests_selenium.py
@@ -241,7 +241,7 @@ class TestImmichWeb(BaseCase):
 
         self.assertEqual(grass['type'], "IMAGE")
         self.assertEqual(grass['exifInfo']['model'], "Pixel 4")
-        self.assertEqual(grass['exifInfo']['dateTimeOriginal'], "2023-07-08T12:13:53.000Z")
+        self.assertEqual(grass['exifInfo']['dateTimeOriginal'], "2023-07-08T12:13:53.210Z")
         self.assertEqual(grass['exifInfo']['city'], "Mora")
         self.assertNotEqual(grass['livePhotoVideoId'], None)
 
