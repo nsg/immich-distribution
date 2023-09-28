1780857f22604558fd8e328e5d1e5037fc74bba0
Stefan Berggren <github@enesge.eu>
2023-09-28 09:16:05 +0200

    Revert "Sync feature: Limit sync file delete"

--- src/bin/sync-service.sh
+++ src/bin/sync-service.sh
@@ -21,16 +21,9 @@ done
 
 cat $SNAP/etc/modify-db.sql | $SNAP/bin/psql -d immich
 
-SYNC_FILE_AGE_DELETE_LIMIT="$(snapctl get sync-file-age-delete-limit)"
-if [ -z "$SYNC_FILE_AGE_DELETE_LIMIT" ]; then
-    # Default to 100 years, this more or less disables the feature
-    # This user need to explicitly set this to a lower value to enable it
-    SYNC_FILE_AGE_DELETE_LIMIT=36500
-fi
-
 for KEY in $(snapctl get sync); do
     {
-        IMMICH_API_KEY="$KEY" SYNC_FILE_AGE_DELETE_LIMIT="$SYNC_FILE_AGE_DELETE_LIMIT" $SNAP/usr/local/bin/python3 $SNAP/bin/sync-service.py
+        IMMICH_API_KEY="$KEY" $SNAP/usr/local/bin/python3 $SNAP/bin/sync-service.py
     } &
 done
 
