dcdaa74c58be097b517f0255f84846e8986b9e24
Stefan Berggren <nsg@nsg.cc>
2024-07-06 10:58:42 +0200

    Remove patch logic from update workflow

--- workflows/create-pr-version-bump.sh
+++ workflows/create-pr-version-bump.sh
@@ -53,7 +53,6 @@ git checkout -b $BRANCH_NAME
 # Bump version
 sed -i "s/$OLD_VERSION/$NEW_VERSION/g" snap/snapcraft.yaml
 sed -i "s/$OLD_VERSION/$NEW_VERSION/g" parts/machine-learning/Makefile
-sed -i "s/$OLD_VERSION/$NEW_VERSION/g" parts/immich-web/patches/Makefile
 sed -i "s/$OLD_VERSION/$NEW_VERSION/g" VERSION
 
 git add .
